f5d210288c8e26c81a8589b3289b83982d0356c2
catehuang <s0350228@gmail.com>
2023-01-18 21:57:34 -0700

    add more seeds of fireworks

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const particles = [];
  * Create fireworks at a time, need to take the computer performance into account
  */
 setInterval(() => {
-    for (let i = 0; i < 10; i++)
+    for (let i = 0; i < 15; i++)
     {
         const firework = new Firework();
         fireworks.push(firework);        
